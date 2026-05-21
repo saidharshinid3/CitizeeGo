b736211ba86c0173b3154e202c71243f45e64e62
saidharshinid3 <saidharshinid3@gmail.com>
2026-05-21 13:41:02 +0530

    Update socket.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://citizeego-backend.onrender.com');
+const socket = io('https://citizeego-backend.onrender.com/api');
 
 export default socket;
